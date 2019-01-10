af369ef0a535ff7b33f508fa5251d37fe736860a
Richard <richard@yenforyang.com>
2019-01-10 05:27:53 +0100

    first commit

---
+++ libmyhtml_4.0.5.dsc
@@ -0,0 +1,20 @@
+Format: 3.0 (native)
+Source: libmyhtml
+Binary: libmyhtml, libmyhtml-dev
+Architecture: any
+Version: 4.0.5
+Maintainer: Alexander Fedyashov <a@fedyashov.com>
+Homepage: https://github.com/lexborisov/myhtml
+Standards-Version: 3.9.5
+Vcs-Browser: https://github.com/lexborisov/myhtml
+Vcs-Git: git://github.com/lexborisov/myhtml.git
+Build-Depends: debhelper (>= 9), cmake
+Package-List:
+ libmyhtml deb libs optional arch=any
+ libmyhtml-dev deb libdevel optional arch=any
+Checksums-Sha1:
+ 9a9f7b85553cb37eb9a775ee37ec3abe466c7edc 940540 libmyhtml_4.0.5.tar.xz
+Checksums-Sha256:
+ f39738610a45034cc98ce104d21d52c8e5f91ffa60d101b678adb9122dc3ba28 940540 libmyhtml_4.0.5.tar.xz
+Files:
+ cccdf3b80df1438ece8817e0c8ad86a7 940540 libmyhtml_4.0.5.tar.xz
---
+++ myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/DependInfo.cmake
@@ -0,0 +1,72 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/dev/shm/myhtml/myhtml/source/mycore/incoming.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/mycore/incoming.c.o"
+  "/dev/shm/myhtml/myhtml/source/mycore/myosi.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/mycore/myosi.c.o"
+  "/dev/shm/myhtml/myhtml/source/mycore/mystring.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/mycore/mystring.c.o"
+  "/dev/shm/myhtml/myhtml/source/mycore/mythread.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/mycore/mythread.c.o"
+  "/dev/shm/myhtml/myhtml/source/mycore/thread_queue.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/mycore/thread_queue.c.o"
+  "/dev/shm/myhtml/myhtml/source/mycore/utils.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/mycore/utils.c.o"
+  "/dev/shm/myhtml/myhtml/source/mycore/utils/avl_tree.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/mycore/utils/avl_tree.c.o"
+  "/dev/shm/myhtml/myhtml/source/mycore/utils/mchar_async.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/mycore/utils/mchar_async.c.o"
+  "/dev/shm/myhtml/myhtml/source/mycore/utils/mcobject.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/mycore/utils/mcobject.c.o"
+  "/dev/shm/myhtml/myhtml/source/mycore/utils/mcobject_async.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/mycore/utils/mcobject_async.c.o"
+  "/dev/shm/myhtml/myhtml/source/mycore/utils/mcsimple.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/mycore/utils/mcsimple.c.o"
+  "/dev/shm/myhtml/myhtml/source/mycore/utils/mcsync.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/mycore/utils/mcsync.c.o"
+  "/dev/shm/myhtml/myhtml/source/mycore/utils/mctree.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/mycore/utils/mctree.c.o"
+  "/dev/shm/myhtml/myhtml/source/mycore/utils/mhash.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/mycore/utils/mhash.c.o"
+  "/dev/shm/myhtml/myhtml/source/myencoding/detect.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myencoding/detect.c.o"
+  "/dev/shm/myhtml/myhtml/source/myencoding/encoding.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myencoding/encoding.c.o"
+  "/dev/shm/myhtml/myhtml/source/myencoding/mystring.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myencoding/mystring.c.o"
+  "/dev/shm/myhtml/myhtml/source/myhtml/callback.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myhtml/callback.c.o"
+  "/dev/shm/myhtml/myhtml/source/myhtml/charef.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myhtml/charef.c.o"
+  "/dev/shm/myhtml/myhtml/source/myhtml/data_process.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myhtml/data_process.c.o"
+  "/dev/shm/myhtml/myhtml/source/myhtml/myhtml.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myhtml/myhtml.c.o"
+  "/dev/shm/myhtml/myhtml/source/myhtml/mynamespace.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myhtml/mynamespace.c.o"
+  "/dev/shm/myhtml/myhtml/source/myhtml/mystring.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myhtml/mystring.c.o"
+  "/dev/shm/myhtml/myhtml/source/myhtml/parser.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myhtml/parser.c.o"
+  "/dev/shm/myhtml/myhtml/source/myhtml/rules.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myhtml/rules.c.o"
+  "/dev/shm/myhtml/myhtml/source/myhtml/serialization.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myhtml/serialization.c.o"
+  "/dev/shm/myhtml/myhtml/source/myhtml/stream.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myhtml/stream.c.o"
+  "/dev/shm/myhtml/myhtml/source/myhtml/tag.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myhtml/tag.c.o"
+  "/dev/shm/myhtml/myhtml/source/myhtml/tag_init.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myhtml/tag_init.c.o"
+  "/dev/shm/myhtml/myhtml/source/myhtml/token.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myhtml/token.c.o"
+  "/dev/shm/myhtml/myhtml/source/myhtml/tokenizer.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myhtml/tokenizer.c.o"
+  "/dev/shm/myhtml/myhtml/source/myhtml/tokenizer_doctype.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myhtml/tokenizer_doctype.c.o"
+  "/dev/shm/myhtml/myhtml/source/myhtml/tokenizer_end.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myhtml/tokenizer_end.c.o"
+  "/dev/shm/myhtml/myhtml/source/myhtml/tokenizer_script.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myhtml/tokenizer_script.c.o"
+  "/dev/shm/myhtml/myhtml/source/myhtml/tree.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myhtml/tree.c.o"
+  "/dev/shm/myhtml/myhtml/source/myport/posix/mycore/io.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myport/posix/mycore/io.c.o"
+  "/dev/shm/myhtml/myhtml/source/myport/posix/mycore/memory.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myport/posix/mycore/memory.c.o"
+  "/dev/shm/myhtml/myhtml/source/myport/posix/mycore/perf.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myport/posix/mycore/perf.c.o"
+  "/dev/shm/myhtml/myhtml/source/myport/posix/mycore/thread.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myport/posix/mycore/thread.c.o"
+  "/dev/shm/myhtml/myhtml/source/myport/posix/mycore/utils/mcsync.c" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/CMakeFiles/myhtml.dir/source/myport/posix/mycore/utils/mcsync.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Intel")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_POSIX_C_SOURCE=199309L"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/libmyhtml.so" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/libmyhtml.so.4.0.5"
+  "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/libmyhtml.so.4" "/dev/shm/myhtml/myhtml/obj-x86_64-linux-gnu/libmyhtml.so.4.0.5"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
